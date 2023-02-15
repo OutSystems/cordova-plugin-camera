f41a08742f74fed6d52c4eea8d928e8301817b1f
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2023-02-15 14:25:00 +0000

    refactor: remove unnecessary imports

--- src/android/CameraLauncher.kt
+++ src/android/CameraLauncher.kt
@@ -23,14 +23,11 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.database.Cursor
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Base64
 import android.util.Log
 import androidx.core.content.FileProvider
 import com.outsystems.plugins.camera.controller.*
@@ -41,7 +38,6 @@ import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.*
-import java.security.Permission
 import java.util.*
 
 /**
