deb7c3f2fec4edd52c44e5fecca0f082d6a397dd
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2025-05-14 17:04:28 +0100

    fix: npx cap sync command

--- add_dependency_capacitor.js
+++ add_dependency_capacitor.js
@@ -29,8 +29,8 @@ if (!pkg.dependencies[NAME]) {
   console.log(`➕ Adding ${NAME}@${SPEC} to app package.json`);
   pkg.dependencies[NAME] = SPEC;
   fs.writeFileSync(APP_PKG, JSON.stringify(pkg, null, 2) + '\n');
-  console.log(`Re-running capacitor sync for `);
-  const syncResult = spawnSync('npx', [`cap sync ${platform}`], {
+  console.log(`Re-running capacitor sync for ${platform}`);
+  const syncResult = spawnSync(`npx cap sync ${platform}`, [], {
     cwd: appDirectory,
     stdio: 'inherit'
   });
