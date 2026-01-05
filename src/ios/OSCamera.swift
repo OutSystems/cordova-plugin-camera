9ed4240e085dfeadce7752375b971978914e5478
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2026-01-05 14:28:56 -0400

    chore: remove unnecessary OSCommonPluginLib
    References: https://outsystemsrd.atlassian.net/browse/RMET-4899

--- src/ios/OSCamera.swift
+++ src/ios/OSCamera.swift
@@ -1,5 +1,4 @@
 import OSCameraLib
-import OSCommonPluginLib
 
 @objc(OSCamera)
 class OSCamera: CDVPlugin {
@@ -134,9 +133,7 @@ class OSCamera: CDVPlugin {
             }
         }
     }
-}
-
-extension OSCamera: PlatformProtocol {
+    
     func sendResult(result: String? = nil, error: NSError? = nil, callBackID: String) {
         var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
 
